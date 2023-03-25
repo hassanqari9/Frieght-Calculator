b78a9ba64587756f0e1edd2c90947f36e195cde4
Alpha <syedaarishshah@gmail.com>
2023-03-25 21:11:20 +0530

    minor fixes in index.js

--- calculator.js
+++ calculator.js
@@ -1,11 +1,9 @@
-const baseRate = 9 // from previous file
-const weight = 100 // from user, from previous file
-const oda = 800
+// Data from previous file
+const baseFreight = 10;
+const weight = 1000;
+const oda = 1000;
 
-
-totalFreight() // this will be base x weight
-odaconditions() // 
-
-if (condition) {
-    
+const totalFreight = totalFreight() // this fn returns Total Freight using the formula Total Freight = (Base Freight x Weight)
+{
+    return (baseFreight * weight);
 }
